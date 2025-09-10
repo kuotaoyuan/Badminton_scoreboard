c7e10366d000bd3d3e4aefb772c62331bdd6c937
kuotaoyuan <kuoian0330xy@gmail.com>
2025-09-10 16:43:16 +0800

    add find_angle function

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -1,11 +1,26 @@
 import mediapipe as mp
 import cv2
+import math
 
 cap = cv2.VideoCapture(0)
 
 my_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+# Find angles between joints using cos(theta) = (a.b)/(|a||b|)
+def find_angle(v1,v2):
+    v1_x = v1[0]
+    v1_y = v1[1]
+    v2_x = v2[0]
+    v2_y = v2[1]
+    dot = v1_x * v2_x + v1_y * v2_y
+    length = math.sqrt(v1_x ** 2 + v1_y ** 2) * math.sqrt(v2_x ** 2 + v2_y ** 2)
+    try:
+        angle = math.degrees(math.acos(dot/length))
+    except:
+        angle = 180
+    return angle
 
 with my_hands.Hands(
     model_complexity = 0,
